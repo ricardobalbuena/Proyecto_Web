46e20cd477ca39b62d065eec4ce104e87a2d3295
ricardobalbuena <29905435+ricardobalbuena@users.noreply.github.com>
2022-07-14 19:43:23 -0400

    Creacion de repositorio

---
+++ README.md
@@ -0,0 +1,19 @@
+# eLibrary
+
+### Uso
+
+```sh
+$ npm install
+```
+
+```sh
+$ npm start
+
+```
+
+```sh
+$ localhost:5000
+
+```
+
+
---
+++ config/keys.js
@@ -0,0 +1,5 @@
+dbPassword = 'mongodb+srv://ricardob:'+ encodeURIComponent('QWERTY') + 'cluster0.zzibc.mongodb.net/?retryWrites=true&w=majority';
+
+module.exports = {
+    mongoURI: dbPassword
+};
